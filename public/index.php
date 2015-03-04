bbcff30b2035ae46d81a9d30b71a13f74ae82dc2
cuevae <cueva.e@hotmail.com>
2015-03-04 19:46:47 +0000

    numbers

--- public/index.php
+++ public/index.php
@@ -10,9 +10,14 @@ $app = new Application();
 
 $app->get('/', function(Request $request, Application $app){
 
-    $question = $request->request->get('q');
+    $question = $request->get('q');
+// ?q=49c14a70:%20which%20of%20the%20following%20numbers%20is%20the%20largest:%2052,%20812
 
-    return new Response('cuevae');
+
+    $var = explode(':', $question);
+    $numbers = explode(',', $var[2]);
+
+    return new Response(max($numbers));
 });
 
 $app->run();
